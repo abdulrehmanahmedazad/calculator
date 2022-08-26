85f57af704b936b7af19457f2857d04a1d7aca35
abdulrehmanahmedazad <110079072+abdulrehmanahmedazad@users.noreply.github.com>
2022-08-26 07:28:31 -0700

    Add files via upload

---
+++ calculator/app.js
@@ -0,0 +1,18 @@
+function cal(b){
+var a=document.getElementById('iui')
+a.value +=b
+}
+
+function result(){
+  var y=  document.getElementById('iui')
+y.value=eval(y.value)
+}
+
+function clear(){
+  var r=document.getElementById('iui')
+  r.value +=''
+}
+
+
+
+
